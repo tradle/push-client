d689fc4c3e225654c4b8b0c4a175b12aacf41dd8
Mark Vayngrib <mark@tradle.io>
2016-07-27 19:02:06 -0400

    simplify API, export only push method

--- example/example.js
+++ example/example.js
@@ -20,18 +20,18 @@ const client = require('../')({
   url: 'http://localhost:48284'
 })
 
-client.register(function (err) {
+const hash = 'd851c4a5a82085e22b7e176a9632f30c1167e47981915cf22c7a95d632026099'
+console.log('pushing to ' + hash)
+
+// assumuing user with link 'e660c830b7d2c440785cdae99b9e6acbad59209f41f7feee406e4030e6d0806a'
+// subscribed to your push notifications
+// client.push('f6a8f59ddb94f02559f76212f9673ab9e3860518db9eeb4e5a0f602a409a6e9c', function (err) {
+client.push(hash, function (err) {
+// client.push("04825784527d1e7ff622810b76252abea1f208bd9bdd2b82db9a0b2aede51954", function (err) {
+// client.push('e660c830b7d2c440785cdae99b9e6acbad59209f41f7feee406e4030e6d0806a', function (err) {
   if (err) throw err
 
-  // assumuing user with link 'e660c830b7d2c440785cdae99b9e6acbad59209f41f7feee406e4030e6d0806a'
-  // subscribed to your push notifications
-  client.push('d851c4a5a82085e22b7e176a9632f30c1167e47981915cf22c7a95d632026099', function (err) {
-  // client.push("04825784527d1e7ff622810b76252abea1f208bd9bdd2b82db9a0b2aede51954", function (err) {
-  // client.push('e660c830b7d2c440785cdae99b9e6acbad59209f41f7feee406e4030e6d0806a', function (err) {
-    if (err) throw err
-
-    console.log('pushed!')
-  })
+  console.log('pushed!')
 })
 
-console.log('link: ' + protocol.linkString(identity))
+console.log('me: ' + protocol.linkString(identity))
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
 const crypto = require('crypto')
 const request = require('superagent')
+const subdown = require('subleveldown')
+const thunky = require('thunky')
 const protocol = require('@tradle/protocol')
 // const constants = require('@tradle/engine').constants
 // const TYPE = constants.TYPE
@@ -11,47 +13,41 @@ module.exports = function (opts) {
     throw new Error('expected db to have json encoding')
   }
 
+  const subscribers = subdown(db, 'subscribers', { valueEncoding: 'json' })
+  const registrar = subdown(db, 'registrar', { valueEncoding: 'json' })
   const serverUrl = opts.url
   const key = opts.key
   const identity = opts.identity
   const publisher = protocol.linkString(identity)
+
+  const maybeRegister = thunky(function (cb) {
+    if (opts.forceRegister) return register(cb)
+
+    registrar.get(serverUrl, function (err) {
+      if (!err) return cb()
+
+      register(cb)
+    })
+  })
+
   return {
-    register: function (cb) {
-      request.post(`${serverUrl}/publisher`)
-        .send({
-          identity: identity,
-          key: key.toJSON()
-        })
-        .end(function (err, res) {
-          if (err || !res.ok) return cb(err || new Error('registration failed on step 1'))
-
-          // challenge
-          const nonce = res.text
-          const salt = crypto.randomBytes(32).toString('base64')
-          key.sign(sha256(nonce + salt), function (err, sig) {
-            if (err) return cb(err)
-
-            request
-              .post(`${serverUrl}/publisher`)
-              .send({ nonce, salt, sig })
-              .end(function (err, res) {
-                if (err || !res.ok) return cb(err || new Error('registration failed on step 2'))
-
-                cb()
-              })
-          })
-        })
-    },
+    // register: maybeRegister,
     // silent notifications only
-    push: function (subscriber, cb) {
-      db.get(subscriber, function (err, info) {
+    push: push
+  }
+
+  function push (subscriber, cb) {
+    maybeRegister(function (err) {
+      if (err) return cb(err)
+
+      subscribers.get(subscriber, function (err, info) {
         if (err) {
           if (!err.notFound) return cb(err)
         }
 
         info = info || { seq: -1 }
         const seq = ++info.seq
-        db.put(subscriber, info, function (err) {
+        subscribers.put(subscriber, info, function (err) {
           if (err) console.log(err)
         })
 
@@ -65,11 +61,38 @@ module.exports = function (opts) {
             .end(function (err, res) {
               if (err || !res.ok) return cb(err || new Error('push failed'))
 
+              registrar.put(serverUrl, {}, cb)
+            })
+        })
+      })
+    })
+  }
+
+  function register (cb) {
+    request.post(`${serverUrl}/publisher`)
+      .send({
+        identity: identity,
+        key: key.toJSON()
+      })
+      .end(function (err, res) {
+        if (err || !res.ok) return cb(err || new Error('registration failed on step 1'))
+
+        // challenge
+        const nonce = res.text
+        const salt = crypto.randomBytes(32).toString('base64')
+        key.sign(sha256(nonce + salt), function (err, sig) {
+          if (err) return cb(err)
+
+          request
+            .post(`${serverUrl}/publisher`)
+            .send({ nonce, salt, sig })
+            .end(function (err, res) {
+              if (err || !res.ok) return cb(err || new Error('registration failed on step 2'))
+
               cb()
             })
         })
       })
-    }
   }
 }
 
